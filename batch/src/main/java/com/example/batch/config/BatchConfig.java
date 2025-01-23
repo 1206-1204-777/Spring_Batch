cb4e46e7267a6abdf271cebb7c71d95bf7fb8c91
Mamy <xingyanjintian@gmail.com>
2025-01-23 12:53:56 +0900

    記述内容を現在のバージョンに対応する形式に修正

--- batch/src/main/java/com/example/batch/config/BatchConfig.java
+++ batch/src/main/java/com/example/batch/config/BatchConfig.java
@@ -1,19 +1,20 @@
 package com.example.batch.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Configuration;
-//Batchのクラスであることを示す
-@Configuration 
-//Batch機能の有効化
-@EnableBatchProcessing 
-public class BatchConfig {
-	private final JobBuilderFactory jobBuilderFactory;
-	private final StepBuilderFactory stepBuilderFactory;
+import org.springframework.transaction.PlatformTransactionManager;
 
-	//コンストラクタで必要なビルダーファクトリーを注入
-	public BatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-		this.jobBuilderFactory = jobBuilderFactory;
-		this.stepBuilderFactory = stepBuilderFactory;
-		
+// Spring Batchの設定クラスであることを示す
+@Configuration
+// Batch機能を有効化
+@EnableBatchProcessing
+public class BatchConfig {
+	private final JobRepository jobRepository;
+	private final PlatformTransactionManager transactionManager;
+	
+	public BatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		this.jobRepository = jobRepository;
+		this.transactionManager = transactionManager;
 	}
 }
