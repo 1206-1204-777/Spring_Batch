9d1c6d02ed4d78f3d319a7179699ff424016c2b6
Mamy <xingyanjintian@gmail.com>
2025-01-23 16:31:04 +0900

    BachConfigでStepメソッドとJobメソッドを定義

--- batch/src/main/java/com/example/batch/config/BatchConfig.java
+++ batch/src/main/java/com/example/batch/config/BatchConfig.java
@@ -1,7 +1,12 @@
 package com.example.batch.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -13,8 +18,25 @@ public class BatchConfig {
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
 	
+	//コンストラクタで必要なビルダーファクトリーを注入
 	public BatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		this.jobRepository = jobRepository;
 		this.transactionManager = transactionManager;
+	}
+	
+	//stepの定義
+	@Bean
+	public Step stepProcess() {
+		return new StepBuilder("stepProcess",jobRepository)
+				.tasklet(new Hellotasklet(),transactionManager)
+				.build();
+	}
+	
+	//jobを定義
+	@Bean
+	public Job jobProcess() {
+		return new JobBuilder("jobProcess",jobRepository)
+				.start(stepProcess())
+				.build();
 	}
 }
---
+++ batch/src/main/java/com/example/batch/config/Hellotasklet.java
@@ -0,0 +1,5 @@
+package com.example.batch.config;
+
+public class Hellotasklet {
+
+}
